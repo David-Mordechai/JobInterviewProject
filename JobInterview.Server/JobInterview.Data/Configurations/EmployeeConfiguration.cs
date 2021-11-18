63601a3584c88aa5d7bed7db5c8b569a43463a0c
David Mordechai <david.mordechai79@gmail.com>
2021-11-18 18:47:53 +0200

    refactoring

--- JobInterview.Server/JobInterview.Data/Configurations/EmployeeConfiguration.cs
+++ JobInterview.Server/JobInterview.Data/Configurations/EmployeeConfiguration.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace JobInterview.Data.Configurations
 {
-    public class EmployeeConfiguration : IEntityTypeConfiguration<Employee>
+    internal class EmployeeConfiguration : IEntityTypeConfiguration<Employee>
     {
         public void Configure(EntityTypeBuilder<Employee> builder)
         {
