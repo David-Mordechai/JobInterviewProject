74c024cb85dcc20ae1fb1bc4bb5e234b7371078a
David Mordechai <david.mordechai79@gmail.com>
2021-11-18 18:22:22 +0200

    JobInterview project - first commit

---
+++ JobInterview.Server/JobInterview.WebApi/Controllers/EmployeesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace JobInterview.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly ILogger<EmployeesController> _logger;
+
+        public EmployeesController(ILogger<EmployeesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "EmployeeController";
+        }
+    }
+}
